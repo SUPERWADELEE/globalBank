6fc7407fbb1e0825baaddb3544724fc280ef3dd1
李威德 <liweide@Mac.localdomain>
2025-05-21 11:02:24 +0800

    feat: 編輯角色寫進操作紀錄

--- app/Filament/Resources/RoleResource/Pages/EditRole.php
+++ app/Filament/Resources/RoleResource/Pages/EditRole.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\RoleResource\Pages;
 use App\Filament\Resources\RoleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Auth;
 
 class EditRole extends EditRecord
 {
@@ -16,4 +17,37 @@ class EditRole extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+    protected function afterSave(): void
+{
+    $role = $this->record;
+
+    $originalPermissions = $role->getOriginal('permissions') ?? [];
+    $newPermissions = $role->permissions->pluck('name')->toArray();
+
+    // 對照差異
+    $added = array_diff($newPermissions, $originalPermissions);
+    $removed = array_diff($originalPermissions, $newPermissions);
+
+    $changes = [];
+
+    if ($added) {
+        $changes[] = __('role.permission_added') . implode('、', $added);
+    }
+
+    if ($removed) {
+        $changes[] = __('role.permission_removed') . implode('、', $removed);
+    }
+
+    if (!empty($changes)) {
+        activity()
+            ->performedOn($role)
+            ->causedBy(Auth::user())
+            ->withProperties([
+                'added_permissions' => $added,
+                'removed_permissions' => $removed,
+            ])
+            ->event('updated')
+            ->log(__('role.permission_updated_log') . implode('；', $changes));
+    }
+}
 }
