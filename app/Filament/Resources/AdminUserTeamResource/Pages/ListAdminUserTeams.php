7a3ea13db9762a9d79d99ed67ffe756693742e32
fix: AdminUserResource form 表單欄位，刪除email及locale欄位，增加密碼隨機產生功能及密碼再次驗證 <liweide@liweidedeMacBook-Air.local>
2025-05-23 20:09:53 +0800

    fix: ListAdminUserTeam 大小標題更改

--- app/Filament/Resources/AdminUserTeamResource/Pages/ListAdminUserTeams.php
+++ app/Filament/Resources/AdminUserTeamResource/Pages/ListAdminUserTeams.php
@@ -9,6 +9,15 @@ use Filament\Resources\Pages\ListRecords;
 class ListAdminUserTeams extends ListRecords
 {
     protected static string $resource = AdminUserTeamResource::class;
+    public function getHeading(): string
+    {
+        return __('system_management.title');
+    }
+
+    public function getSubheading(): string
+    {
+        return __('admin_user.team.title');
+    }
 
     protected function getHeaderActions(): array
     {
@@ -28,8 +37,8 @@ class ListAdminUserTeams extends ListRecords
                 ->label(__('admin_user.navigation.team_management')),
         ];
     }
-    public function getTitle(): string
-    {
-        return __('admin_user.team.title');
-    }
+    // public function getTitle(): string
+    // {
+    //     return __('admin_user.team.title');
+    // }
 }
