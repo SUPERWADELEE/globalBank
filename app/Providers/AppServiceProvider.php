f925db31c3c0d62fd45a01d3e337d7119ce6138f
fix: AdminUserResource form 表單欄位，刪除email及locale欄位，增加密碼隨機產生功能及密碼再次驗證 <liweide@liweidedeMacBook-Air.local>
2025-05-24 15:10:14 +0800

    fix: 拿掉RatePolice在AppProvider 的註冊

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,14 +6,6 @@ use Illuminate\Support\ServiceProvider;
 use Filament\Support\Facades\FilamentView;
 use Illuminate\Contracts\View\View;
 use Filament\View\PanelsRenderHook;
-use Spatie\Permission\Models\Permission;
-use Illuminate\Support\Facades\Gate;
-use App\Models\Wallet;
-use App\Policies\UserWalletPolicy;
-use App\Models\Rate;
-use App\Policies\RatePolicy;
-use Illuminate\Contracts\Auth\Authenticatable;
-use App\Models\AdminUser;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -37,9 +29,7 @@ class AppServiceProvider extends ServiceProvider
             fn(): View => view('filament.partials.custom-header-icon')
         );
 
-        Gate::policy(Rate::class,   RatePolicy::class);
-        
-        // 註冊自定義的 Export 模型
+        // 針對匯出的驗證，覆寫覆聯的user對象為admin_user
         $this->app->bind(\Filament\Actions\Exports\Models\Export::class, \App\Models\Export::class);
     }
 }
