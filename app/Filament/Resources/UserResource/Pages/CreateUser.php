164382026c431f83283eb97fba42d5cfd5c06ca3
wadelee <liweide@liweidedeMacBook-Air.local>
2025-05-26 21:10:07 +0800

    refactor: 優化新增會員時新增錢包的邏輯

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -7,40 +7,20 @@ use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use App\Models\Wallet;
+use App\Models\CurrencyCode;
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
 
     protected function afterCreate(): void
     {
-       $now = Carbon::now();
-       DB::table('wallets')->insert([
-        'user_id' => $this->record->id,
-        'currency_code_id' => 1,
-        'balance' => 0,
-        'created_at' => $now,
-        'updated_at' => $now,
-       ]);
-       DB::table('wallets')->insert([
-        'user_id' => $this->record->id,
-        'currency_code_id' => 3,
-        'balance' => 0,
-        'created_at' => now(),
-        'updated_at' => now(),
-       ]);
-       DB::table('wallets')->insert([
-        'user_id' => $this->record->id,
-        'currency_code_id' => 2,
-        'balance' => 0,
-        'created_at' => $now,
-        'updated_at' => $now,
-       ]);
-       DB::table('wallets')->insert([
-        'user_id' => $this->record->id,
-        'currency_code_id' => 4,
-        'balance' => 0,
-        'created_at' => $now,
-        'updated_at' => $now,
-       ]);
+        foreach (CurrencyCode::all() as $currency) {
+            Wallet::create([
+                'user_id' => $this->record->id,
+                'currency_code_id' => $currency->id,
+                'balance' => 0,
+            ]);
+        }
     }
 }
