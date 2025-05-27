b3e9e69719b3e51624a199a80800d40d17852f43
wadelee <liweide@Mac.localdomain>
2025-05-27 11:01:26 +0800

    fix: 補上補上usdt設定的英文翻譯

---
+++ resources/lang/en/usdt_setting.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'navigation' => [
+        'usdt_settings' => 'USDT Settings',
+        'fee_settings' => 'Fee Settings',
+        'edit_fee_settings' => 'Edit Fee Settings',
+    ],
+    'fee_amount' => 'Fee Amount',
+];
