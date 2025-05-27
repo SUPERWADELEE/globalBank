5d59a3d626f4e2da7e0e85f934aa020d13b65d57
wadelee <liweide@Mac.localdomain>
2025-05-27 11:04:42 +0800

    fix: 補上安裝資訊頁面的中英翻譯

--- resources/lang/en/install_info.php
+++ resources/lang/en/install_info.php
@@ -4,4 +4,12 @@ return [
     'title' => 'Installation Information',
     'navigation_label' => 'Installation Information',
     'navigation_group' => 'System Settings',
+    'app_download_info' => 'APP Download Information',
+    'copy_download_image' => 'Copy Download Image',
+    'copy_download_url' => 'Copy Download URL',
+    'frontend_url' => 'Frontend URL',
+    'google_authenticator' => 'Google Authenticator Download and Key',
+    'google_authenticator_qr_code' => 'Google Authenticator QR Code',
+    'copy_qr_code' => 'Copy QR Code',
+    'copy_secret' => 'Copy Secret',
 ]; 
--- resources/lang/zh_TW/install_info.php
+++ resources/lang/zh_TW/install_info.php
@@ -4,5 +4,12 @@ return [
     'title' => '安裝資訊',
     'navigation_label' => '安裝資訊',
     'navigation_group' => '系統設定',
+    'app_download_info' => 'APP下載資訊',
+    'copy_download_image' => '複製下載圖片',
+    'copy_download_url' => '複製下載網址',
+    'frontend_url' => '前台網址',
+    'google_authenticator' => 'Google驗證下載與金鑰',
+    'google_authenticator_qr_code' => 'Google驗證器 QR Code',
+    'copy_qr_code' => '複製QR Code',
 ];
 
