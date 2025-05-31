075038c961ff8ab4e22ed75d74fc3bd3faeddabf
wadelee <liweide@liweidedeMacBook-Air.local>
2025-05-31 15:04:49 +0800

    feat: exchange route livewire blade 架構初始化

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Livewire\Front\Member\Wallet;
 use App\Livewire\Front\Auth\Login;
 use App\Livewire\Front\Member\Withdraw;
 use App\Livewire\Front\Member\Deposit;
+use App\Livewire\Front\Member\Exchange;
 
 
 Route::get('/login', Login::class)->name('login');
@@ -14,6 +15,7 @@ Route::prefix('member')->name('member.')->group(function () {
     Route::get('/wallet', Wallet::class)->name('wallet');
     Route::get('/withdraw', Withdraw::class)->name('withdraw');
     Route::get('/deposit', Deposit::class)->name('deposit');
+    Route::get('/exchange', Exchange::class)->name('exchange');
 }); 
 Route::get('/', function () {
     return redirect()->route('login');
