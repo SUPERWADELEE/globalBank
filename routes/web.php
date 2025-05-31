eae73a474d0ed4dbda2744318ce312740ffdb789
wadelee <liweide@liweidedeMacBook-Air.local>
2025-05-31 14:33:59 +0800

    feat: login route livewire blade 架構初始化

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Front\Auth\Login;
 
 
+Route::get('/login', Login::class)->name('login');
 
+Route::get('/', function () {
+    return redirect()->route('login');
+});
