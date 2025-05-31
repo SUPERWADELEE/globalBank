7c4c63f1c699a3274da2e6b05c13a2879d838c2e
wadelee <liweide@liweidedeMacBook-Air.local>
2025-05-31 15:03:23 +0800

    feat: withdraw route livewire blade 架構初始化

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Front\Member\Wallet;
 use App\Livewire\Front\Auth\Login;
+use App\Livewire\Front\Member\Withdraw;
 use App\Livewire\Front\Member\Deposit;
 
 
@@ -11,6 +12,7 @@ Route::get('/login', Login::class)->name('login');
 // 會員區域路由 - 明確指定使用 web guard(要讓前端切版後續再加)
 Route::prefix('member')->name('member.')->group(function () {
     Route::get('/wallet', Wallet::class)->name('wallet');
+    Route::get('/withdraw', Withdraw::class)->name('withdraw');
     Route::get('/deposit', Deposit::class)->name('deposit');
 }); 
 Route::get('/', function () {
